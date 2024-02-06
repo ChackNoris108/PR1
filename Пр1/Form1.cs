72b518bd5d0e420ca05e750e4330f8da560cc1d7
ChackNoris108 <159127489+ChackNoris108@users.noreply.github.com>
2024-02-06 12:50:12 +0300

    Add files via upload

---
+++ Пр1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Пр1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private string GetTriangleType(int a, int b, int c)
+        {
+            if (a == b && b == c)
+            {
+                return "Равносторонний";
+            }
+            else if (a == b || a == c || b == c)
+            {
+                return "Равнобедренный";
+            }
+            else
+            {
+                int[] sides = { a, b, c };
+                Array.Sort(sides);
+                if (sides[2] * sides[2] == sides[0] * sides[0] + sides[1] * sides[1])
+                {
+                    return "Прямоугольный";
+                }
+                else if (sides[2] * sides[2] > sides[0] * sides[0] + sides[1] * sides[1])
+                {
+                    return "Тупоугольный";
+                }
+                else
+                {
+                    return "Остроугольный";
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                int sideA = int.Parse(textBox1.Text);
+                int sideB = int.Parse(textBox2.Text);
+                int sideC = int.Parse(textBox3.Text);
+
+                if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                {
+                    throw new ArgumentException("Стороны треугольника должны быть положительными числами.");
+                }
+
+                if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+                {
+                    throw new ArgumentException("Такой треугольник не существует.");
+                }
+
+                string triangleType = GetTriangleType(sideA, sideB, sideC);
+                double perimeter = sideA + sideB + sideC;
+                double halfPerimeter = perimeter / 2;
+                double area = Math.Sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC));
+
+                MessageBox.Show($"Тип треугольника: {triangleType}\n" +
+                                   $"Периметр треугольника: {perimeter}\n" +
+                                   $"Площадь треугольника: {area}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите целочисленные значения для сторон треугольника.");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 a = new Form2();
+            Hide();
+            a.Show();
+        }
+    }
+}
---
+++ Пр1/Form2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Math;
+
+namespace Пр1
+{
+    public partial class Form2 : Form
+    {
+       
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsValidInput())
+            {
+
+
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                double c = Convert.ToDouble(textBox3.Text);
+
+                double ot = b * b - 4 * a * c;
+
+                label7.Text = "Дискриминант равен: " + ot;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            label9.Text = "";
+            label8.Text = "";
+            label7.Text = "";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsValidInput())
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                double c = Convert.ToDouble(textBox3.Text);
+                double ot = b * b - 4 * a * c;
+
+                if (ot > 0)
+                {
+                    double x1 = (-b + Sqrt(ot)) / 2 * a;
+                    double x2 = (-b - Sqrt(ot)) / 2 * a;
+                    label8.Text = "x1= " + x1;
+                    label9.Text = "x2=" + x2;
+                    label10.Text = "2";
+                }
+                else if (ot == 0)
+                {
+                    double x1 = -(b / (2 * a));
+                    label8.Text = "x1= " + x1;
+                    label10.Text = "1";
+                }
+                else if (ot < 0)
+                {
+                    label8.Text = "Нет корней";
+                    label10.Text = "0";
+                }
+            }
+            
+        }
+        private bool IsValidInput()
+        {
+            if (!double.TryParse(textBox1.Text, out double a) ||
+                !double.TryParse(textBox2.Text, out double b) ||
+                !double.TryParse(textBox3.Text, out double c))
+            {
+                MessageBox.Show("Пожалуйста, введите Числа во все три поля.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
